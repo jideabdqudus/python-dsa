3d6d84618e0cb4a5a755a6de769947afef6004d9
jideabdqudus <jideabdqudus@gmail.com>
2021-08-24 16:26:27 +0100

    Water Container, Reverse Array, 2 Pointer technique

--- container_water.py
+++ container_water.py
@@ -1,5 +1,4 @@
-arr = [7,1,2,3,9]
-
+# Working Solution
 def rain_water(arr):
     if (len(arr) < 2):
         print(0)
@@ -13,4 +12,45 @@ def rain_water(arr):
     print(value)
     return value
 
-rain_water(arr)
+# Optimal Solution
+def rain_water_two(arr):
+    if (len(arr) < 2):
+        print(0)
+        return 0
+    p1, p2, maxArea = 0, len(arr)-1, 0
+    while p1 < p2:
+        height = min(arr[p1], arr[p2])
+        width = p2 - p1
+        area = height * width
+        maxArea = max(area, maxArea)
+
+        if arr[p1] < arr[p2]:
+            p1 += 1
+        else:
+            p2 -= 1
+    print(maxArea)
+    return maxArea
+
+# Reversing array using 2 pointer method
+def reverseArray(array):
+    start, end = 0, len(array) - 1
+    while start < end:
+        array[start], array[end] = array[end], array[start]
+        start += 1
+        end -= 1
+    print(array)
+
+# Nested For loop
+def movingLoop(arr):
+    for x in range(len(arr)):
+        for i in range(x+1, len(arr)):
+            print(x, i)
+
+
+
+
+array = [10, 20, 30, 40, 50]
+reverseArray(array)
+movingLoop(array)
+rain_water(array)
+rain_water_two(array)
